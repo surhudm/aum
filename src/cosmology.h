7f2e2b24d443487aef13317adbd73a8ef4bd9999
Surhud More <surhud.more@ipmu.jp>
2014-12-15 23:38:54 +0900

    Moving public and private functions around for readability

--- src/cosmology.h
+++ src/cosmology.h
@@ -75,6 +75,8 @@ double dxilbar(double,void*);
 double dxilbarbar(double,void*);
 double dwpnl_kaiser(double, void*);
 double dwpl_kaiser(double, void*);
+double dvar_G(double x, void * params);
+double dvar_TH(double x, void * params);
 
 class cosmology
 {
@@ -244,40 +246,66 @@ class cosmology
     gsl_interp_accel *xiL_barbar_acc;
     gsl_spline *xiL_barbar_spline;
 
+    /// f_g(M) swindle
+    double fgm_m0;
+    double fgm_slp;
+
 
     /// Private functions
     void initialize();          //Initializations
-    //double Lookback(double);    //Lookback time(z) units 1/H0
-    //double Time(double);        //Time(z) units 1/H0
-    //double Eofz(double);        //Eofz(z) 
-    double Omega(double);       //Omega(z) 
-    double Omegaw(double);       //Omegaw(z) 
-    double Delta_crit(double);  //Delta_crit(z), Bryan and Norman '98
+    double Chiofz(double);    //Comoving distance h^{-1} Mpc for flat cosmology, Chi for non-flat cosmology
     double growthfactor(double);  //Growth factor dummy
 
-    void init_growthfactor();   //Speed up for growth factor calculations
-    void init_Chi();   //Speed up for growth factor calculations
+    void init_growthfactor();   // Speed up for growth factor calculations
+    void init_Chi();            // Speed up comoving distance calculations
     double initPS_EH();         // Initialize Power spectrum variables
 
     void fixd2norm();                // Fix the power spectrum normalization
+
+
+    void init_powerspectra_NL(double); //Speed up for power spectra calculations
+    void init_powerspectra_L(); //Speed up for power spectra calculations
+    double Delta2_L(double,double); // \Delta^2(k) Power spectrum, k should be in units of h Mpc^{-1} wrapper
+    double Delta2_NL(double,double); // \Delta^2(k) Power spectrum, k should be in units of h Mpc^{-1} wrapper
+    double Pk_L(double,double);     // \P(k) Power spectrum, k should be in units of h Mpc^{-1} wrapper
+    double Pk_NL(double,double);    // Non-linear power spectrum wrapper
+
+    // Correlation function calculations: Tests
+    double xi_L(double,double);       // Linear correlation function quadpack
+    double xi_NL(double,double);      // Nonlinear correlation function quadpack
+    double xi_L_old(double,double);   // Linear correlation function gauleg way
+    double xi_NL_old(double,double);  // Nonlinear correlation function gauleg way
+
+    // Global redshift indicator for different splines
+    double z_glob;
+    double gf_glob;
+
+    //Speed up the correlation calculations
+    void init_xi_L(); 
+    void init_xi_NL(double);
+
     double Delta2_EH(double,double); // \Delta^2(k) Power spectrum, k should be in units of h Mpc^{-1}
     double Pk_EH(double,double);     // \P(k) Power spectrum, k should be in units of h Mpc^{-1}
-        double TCold_EH(double);         // EH 98 Transfer function for CDM k in Mpc^{-1} 
-        double Tb_EH(double);            // EH 98 Transfer function for baryons k in Mpc^{-1}
-        double T0master_EH(double,double,double);  // EH 98 Transfer function master for CDM k in Mpc^{-1}
-        double TF_EH(double);            // EH 98 Density-wtd Transfer function for CDM+Baryons k in Mpc^{-1}
-        double var_TH(double,double);    // \sigma^2(R) R in h^{-1} Mpc assuming a top hat filter in real space
-        double var_G(double,double);     // \sigma^2(R) R in h^{-1} Mpc assuming a top hat filter in real space
+    double TCold_EH(double);         // EH 98 Transfer function for CDM k in Mpc^{-1} 
+    double Tb_EH(double);            // EH 98 Transfer function for baryons k in Mpc^{-1}
+    double T0master_EH(double,double,double);  // EH 98 Transfer function master for CDM k in Mpc^{-1}
+    double TF_EH(double);            // EH 98 Density-wtd Transfer function for CDM+Baryons k in Mpc^{-1}
+    double var_TH(double,double);    // \sigma^2(R) R in h^{-1} Mpc assuming a top hat filter in real space
+    double var_G(double,double);     // \sigma^2(R) R in h^{-1} Mpc assuming a top hat filter in real space
+
     double init_Smith(double);           // Initialize the non-linear power spectrum
-        double getksigma(double);        // ksigma for Smith et al. 2003
-        double getneff(double);          // neff for Smith et al. 2003
-        double getC(double);             // C for Smith et al. 2003
+    double getksigma(double);        // ksigma for Smith et al. 2003
+    double getneff(double);          // neff for Smith et al. 2003
+    double getC(double);             // C for Smith et al. 2003
     double Delta2NL_S(double,double);    // Non-linear power spectrum Smith et al. 2003
     double PkNL_S(double,double);        // Non-linear power spectrum Smith et al. 2003
+
     void Delta2_EH_S(double,double,double&,double&); //Both linear and nonlinear power spectrum
     void Pk_EH_S(double,double,double&,double&); //Both linear and nonlinear power spectrum
+
     void init_varM_TH_spline();          // Initialise the numerical calculation of variance
     double varM_TH(double, double);      // Variance of the density field smoothed with a Tophat filter of mass scale M
+
     double MF_WA(double,double);         // Warren et al . mass function now obsolete
     double MF_ST(double,double);         // Sheth Tormen mass function now obsolete
     double MF_BH(double,double);         // Bhattacharya mass function experimental
@@ -301,8 +329,28 @@ class cosmology
     void setrzeta(double);
     double findrzfn(double x, double tgt, double z);
 
+    void ukinit();
+    void ukinit2();
 
-    // Initqk
+
+    // For Kaiser effect correction
+    double xiNL_bar_num(double,double);
+    void init_xiNL_bar(double);
+    double xiNL_bar(double,double);
+
+    double xiNL_barbar_num(double,double);
+    void init_xiNL_barbar(double);
+    double xiNL_barbar(double,double);
+
+    double xiL_bar_num(double,double);
+    void init_xiL_bar(double);
+    double xiL_bar(double,double);
+
+    double xiL_barbar_num(double,double);
+    void init_xiL_barbar(double);
+    double xiL_barbar(double,double);
+
+
 #ifndef SWIG
     const static double hod_kmin=-4.0;
     const static double hod_kmax=7.5;
@@ -349,6 +397,8 @@ class cosmology
     friend double dxilbarbar(double,void*);
     friend double dwpnl_kaiser(double, void*);
     friend double dwpl_kaiser(double, void*);
+    friend double dvar_G(double x, void * params);
+    friend double dvar_TH(double x, void * params);
 
     public:
     
@@ -359,39 +409,30 @@ class cosmology
         cosmology(cosmo); //Constructor
 	void cosmo_free();
 
-        double Chiofz_num(double);    //Comoving distance h^{-1} Mpc
-        double Chiofz(double);    //Comoving distance h^{-1} Mpc
+        // Different distances
+        double Chiofz_num(double);    //Comoving distance h^{-1} Mpc for flat cosmology, Chi for non-flat cosmology
         double Dlofz(double);     //Luminosity distance h^{-1} Mpc
         double Daofz(double);     //Angular diameter distance h^{-1} Mpc
         double Daofzlh(double,double);     //Angular diameter distance h^{-1} Mpc
 
+        // Growth factor and f for redshift space distortions
         double growthfactor_num(double); // Interpolating routine        
         double dlnDdln1pz(double z);
+
+        // Density parameters as a function of redshift
+        double Omega(double);       //Omega(z) 
+        double Omegaw(double);       //Omegaw(z) 
+
+        // Virial overdensity 
+        double Delta_crit(double);  //Delta_crit(z), Bryan and Norman '98
 
 	void set_optmf(int); // Set the mass function option
 
         // Power spectrum calculations
-        double Delta2_L(double,double); // \Delta^2(k) Power spectrum, k should be in units of h Mpc^{-1} wrapper
-        double Delta2_NL(double,double); // \Delta^2(k) Power spectrum, k should be in units of h Mpc^{-1} wrapper
-        double Pk_L(double,double);     // \P(k) Power spectrum, k should be in units of h Mpc^{-1} wrapper
-        double Pk_NL(double,double);    // Non-linear power spectrum wrapper
-
-	void init_powerspectra_NL(double); //Speed up for power spectra calculations
-	void init_powerspectra_L(); //Speed up for power spectra calculations
         double Delta2_L_num(double,double);     // Numerical \Delta^2(k) Power spectrum, k should be in units of h Mpc^{-1} wrapper
         double Delta2_NL_num(double,double);    // Numerical Non-linear \Delta^2(k),  k should be in units of h Mpc^{-1}
 
-
-	// Correlation function calculations: Tests
-	double xi_L(double,double);       // Linear correlation function quadpack
-	double xi_NL(double,double);      // Nonlinear correlation function quadpack
-	double xi_L_old(double,double);   // Linear correlation function gauleg way
-	double xi_NL_old(double,double);  // Nonlinear correlation function gauleg way
-
-        //Speed up the correlation calculations
-	void init_xi_L(); 
 	double xi_L_num(double,double);
-	void init_xi_NL(double);
 	double xi_NL_num(double,double);
 
 	// FT(FT(P(k)))
@@ -408,27 +449,25 @@ class cosmology
 	double varM_TH_num_deriv(double,double);
 
 	// Group catalog related functions
+        // Number density of haloes above mass M
         double Nplus(double, double);
+        // Get mass M to obtain a given Number density of haloes above this mass
         double getM(double,double);
 
         //NFW profile related functions
         void modelNFWhalo(double,double,double&,double&,double&,double&,double&); // Radii in physical units
         void modelNFWhalo_com(double,double,double&,double&,double&,double&,double&); // Mvir, Rvir, cvir, R200, c200
         void modelNFWhalo_com(double,double,double&,double&,double&); //Mvir, Rvir, cvir
+
         double conc(double,double); //Concentration parameter wrapper
         double ukofm(double,double,double); // Fourier transform of NFW profile
         double uskofm(double,double,double,double); // Fourier transform of NFW profile for satellites
 	double ukinterp(double,double); // Interpolation routine to find ukofm quickly
 
-	//Temporarily public
-	void ukinit();
-	void ukinit2();
-    double Eofz(double);        //Eofz(z) 
+        double Eofz(double);        //Eofz(z) 
 
 	/// Set new z
 	void setnew_z(double);
-	double z_glob;
-	double gf_glob;
 
 	/// Access to private variables
 	double gets8();
@@ -451,27 +490,6 @@ class cosmology
     double wpnl(double,double,double);
     double wpl(double z,double rad, double projmax);
 
-    /// f_g(M) swindle
-    double fgm_m0;
-    double fgm_slp;
-
-    // For Kaiser effect correction
-    double xiNL_bar_num(double,double);
-    void init_xiNL_bar(double);
-    double xiNL_bar(double,double);
-
-    double xiNL_barbar_num(double,double);
-    void init_xiNL_barbar(double);
-    double xiNL_barbar(double,double);
-
-    double xiL_bar_num(double,double);
-    void init_xiL_bar(double);
-    double xiL_bar(double,double);
-
-    double xiL_barbar_num(double,double);
-    void init_xiL_barbar(double);
-    double xiL_barbar(double,double);
-
     double wpnl_kaiser(double z,double rad, double projmax,double fkai);
     double xi_NL_kaiser(double x,double arg, double z,double fkai);
 
@@ -481,11 +499,13 @@ class cosmology
     /// New functionality added renew cosmology
     void renew(cosmo p);
  
+    /// Get sound horizon a'la Eisenstein Hu approximation
     double rsound();
+
+    /// Functions to calculate distances given ra and dec
     double get_deltapi(double,double);
     double get_sinsqang(double,double,double,double,double,double);
     double get_logrp(double,double,double,double,double,double,double);
-
 
 };
 
